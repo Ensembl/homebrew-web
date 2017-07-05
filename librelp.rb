c9ed920bd2ae1da4495e3125d99c20bc296a3337
Ensembl User <ensembl-webteam@ebi.ac.uk>
2017-07-05 13:04:05 +0100

    RELP reliable syslog protocol.

---
+++ librelp.rb
@@ -0,0 +1,38 @@
+# Documentation: http://docs.brew.sh/Formula-Cookbook.html
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Librelp < Formula
+  desc "librelp"
+  homepage "http://www.librelp.com/"
+  url "http://download.rsyslog.com/librelp/librelp-1.2.14.tar.gz"
+  sha256 "11f6241a4336358a33bfdadd43ef299e8258db0a5243d0c164499c6b85ae5955"
+
+  # depends_on "cmake" => :build
+  depends_on "gnutls"
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test librelp`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
