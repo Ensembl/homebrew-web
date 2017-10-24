7f35987b30cd0f20bb9020dfbabd6d0b626fc3a7
Andrew Yates <ayates@ebi.ac.uk>
2017-10-24 17:14:28 +0100

    apr and apr-util must be compiled. No kegs

--- apr.rb
+++ apr.rb
@@ -5,15 +5,6 @@ class Apr < Formula
   sha256 "09109cea377bab0028bba19a92b5b0e89603df9eab05c0f7dbd4dd83d48dcebd"
   revision 1
 
-  bottle do
-    cellar :any
-    sha256 "64527b35f69f53202253676c3727411fa3fb3a0ae1b71ca266c62d2a87faf3bd" => :high_sierra
-    sha256 "5a0ef0c7992eaa6fc1ac332f72cdaebfb5fc0e9fd70a00606fc72e3a5c9d0afb" => :sierra
-    sha256 "b5b9ef99e199a850bc0485fec5968ada7dece9d107df3af0014ecb7245cf5f68" => :el_capitan
-    sha256 "cca4cfe47cf5e2f504c2bba59ddb991211b4ed778b2cadda74ea418305a5d942" => :yosemite
-    sha256 "fde87cb48e98a8037fcb208867e04e3dd76a7f3e5361787450d0c1b2b773233b" => :x86_64_linux
-  end
-
   keg_only :provided_by_osx, "Apple's CLT package contains apr"
 
   depends_on "util-linux" => :recommended if OS.linux? # for libuuid
